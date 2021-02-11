348c2269d65e51b594049aa1e24979699bdc124d
Denise Welsch <denise.welsch@reyar.de>
2021-02-11 17:33:06 +0100

    initial

---
+++ global.R
@@ -0,0 +1,18 @@
+MAX_FILE_SIZE_MB <- 1L
+options(shiny.maxRequestSize = MAX_FILE_SIZE_MB*1024^2)
+options(shiny.sanitize.errors = TRUE)
+
+library(shiny)
+library(rmarkdown)
+library(anytime)
+library(mailR)
+library(data.table)
+library(readr)
+library(shinydisconnect)
+
+library(knitr) 
+library(FactoMineR)
+library(FactoInvestigate)
+
+source("chooser.R")
+source("helpers/Functions.R")
